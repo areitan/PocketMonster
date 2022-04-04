a03ed1fa1b99a0c31f001a87bbef6fa5d406783b
Jared Teraoka <jteraoka13@gmail.com>
2022-04-04 13:14:40 -0700

    defined some html elements for initial form

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,18 @@
-console.log('Test')
+    //Initial HTML UI Elements//
+var searchFormEl = document.querySelector("#search-form");
+var submitBtnEl = document.querySelector("#search-btn");
+var recentListEl = document.querySelector("#search-recents");
+var favoriteListEl = document.querySelector("#search-favorites");
+
+    //Attribute Selection//
+
+var alignmentSelect= document.querySelector("#select-alignment");
+var challengeSelect= document.querySelector("#select-cr");
+var typeSelect= document.querySelector("#select-type");
+
+    //Event Listener//
+
+submitBtnEl.addEventListener("click", function(event) {
+    event.preventDefault();
+
+});
