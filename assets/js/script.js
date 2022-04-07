742954b1b91e862084e8cae0e03cb857331196be
Nikolas <kolemazur@gmail.com>
2022-04-06 22:07:40 -0600

    Add qrURL variable

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,5 @@
-
 // Search Button variable
 var searchButton = document.getElementById('searchButton');
+
+// goqr API https://open5e.com/monsters/monster-list //
+var qrURL = "https://api.qrserver.com/v1/create-qr-code/?data=https://open5e.com/monsters/monster-list&size=300x300&format=.svg";
