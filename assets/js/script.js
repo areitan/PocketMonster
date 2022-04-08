b73770fc81b82457a716b3f68aff553908bdd359
Nikolas Mazur <kolemazur@gmail.com>
2022-04-08 08:09:39 -0600

    updated QR code URL

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,7 @@
 var searchButton = document.getElementById('searchButton');
 
 // goqr API https://open5e.com/monsters/monster-list
-var qrURL = "https://api.qrserver.com/v1/create-qr-code/?data=https://open5e.com/monsters/monster-list&size=300x300&format=.svg";
+var qrURL = "https://api.qrserver.com/v1/create-qr-code/?data=https://open5e.com/monsters/" + slug + "&size=300x300&format=.svg";
 var qrGen = document.getElementById("qr");
 // Changes src in empty <img> tag
 qrGen.src = qrURL;
