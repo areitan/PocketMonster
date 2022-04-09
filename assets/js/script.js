531441b95e8a47cf70161a822bf7bfe754a38f80
Nikolas Mazur <kolemazur@gmail.com>
2022-04-09 11:06:09 -0600

    Add slug var

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,9 @@
 // Search Button variable
 var searchButton = document.getElementById('searchButton');
+var slug = monsterSpecs.slug;
 
 // goqr API https://open5e.com/monsters/monster-list
-var qrURL = "https://api.qrserver.com/v1/create-qr-code/?data=https://open5e.com/monsters/" + slug + "&size=300x300&format=.svg";
+var qrURL = "https://api.qrserver.com/v1/create-qr-code/?data=" + slug + "&size=300x300&format=.svg";
 var qrGen = document.getElementById("qr");
 // Changes src in empty <img> tag
 qrGen.src = qrURL;
